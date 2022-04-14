0248e5d4625587bfb3f01a2caa5fde76e42274a6
Vitmyself <vitoraciole@hotmail.com>
2022-04-14 16:19:08 -0300

    Fixing general code

--- project_group.py
+++ project_group.py
@@ -3,17 +3,49 @@ def log_agroup(archive):
     total_kills = 0
     players = []
     kills = {}
+    weapons = {}
 
     archive = open(file=archive, mode="r", encoding="utf8")
     for line in archive:
-        line = archive.readline()
-        if "0:00 InitGame" in line:
-            print(line)
-            game += 1
-        elif "Kill:" in line:
+        if "Kill:" in line:
             total_kills += 1
+            if line.split()[-1] in weapons:
+                weapons[line.split()[-1]] += 1
+            else:
+                weapons[line.split()[-1]] = 1
+            if line.split()[5] in players:
+                pass
+            if line.split()[5] not in players:
+                if line.split()[6] != "killed":
+                    pass
+                    if line.split()[7] != "killed":
+                        list = []
+                        list.append(line.split()[5])
+                        list.append(line.split()[6])
+                        list.append(line.split()[7])
+                        string = ' '.join(list)
+                        if string not in players:
+                            players.append(string)
+            if line.split()[5] not in players:
+                if line.split()[6] != "killed":
+                    pass
+                    if line.split()[7] == "killed":
+                        list = []
+                        list.append(line.split()[5])
+                        list.append(line.split()[6])
+                        string = ' '.join(list)
+                        if string not in players:
+                            players.append(string)
+            if line.split()[5] not in players:
+                if line.split()[6] == "killed":
+                    string = line.split()[5]
+                    if string not in players:
+                        if string != "<world>":
+                            players.append(string)
+        if "0:00 InitGame" in line:
+            game += 1
     archive.close()
-    return ("total_kills = " + str(total_kills), game)
+    return ("total_kills = " + str(total_kills), game, players, weapons, kills)
 
 
 print(log_agroup("qgames.log"))
